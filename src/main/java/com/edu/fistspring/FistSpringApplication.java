89c6bb7987cc579b1b7be3809dad3be61b7d7254
DESKTOP-UQN23HH\QuangLinh <hackcoquanglinh2000@gmail.com>
2023-02-24 09:57:29 +0700

    fist config

---
+++ src/main/java/com/edu/fistspring/FistSpringApplication.java
@@ -0,0 +1,13 @@
+package com.edu.fistspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FistSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FistSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/fistspring/FistSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.fistspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FistSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
